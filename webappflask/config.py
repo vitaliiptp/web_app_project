be413e3c08942e0be54dba458b9781b46dc76c73
vitaliiptp <vitalii.potapenko@gmail.com>
2021-06-04 11:10:32 +0200

    update config for SQLAlchemy

--- webappflask/config.py
+++ webappflask/config.py
@@ -4,6 +4,7 @@ from decouple import config
 class Config:
     SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
